2d0be28400e29fb0c95271dd689cd5245325c0f1
anshulbanwala <80454613+anshulbanwala@users.noreply.github.com>
2024-08-09 21:50:26 +0530

    Create BillService to manage business logic for bills
    - Added methods to interact with BillRepository and perform operations.
    - Implemented business logic for adding, updating, and deleting bills.
    
    - Methods include: addBill(), updateBill(), deleteBill(),

--- src/com/paypilot/service/BillService.java
+++ src/com/paypilot/service/BillService.java
@@ -1,5 +1,34 @@
 package com.paypilot.service;
 
+import com.paypilot.model.Bill;
+import com.paypilot.repo.BillRepository;
+import java.util.List;
+
 public class BillService {
 
+    private BillRepository billRepository;
+
+    public BillService(BillRepository billRepository) {
+        this.billRepository = billRepository;
+    }
+
+    public void addBill(Bill bill) {
+        billRepository.addBill(bill);
+    }
+
+    public void updateBill(Bill bill) {
+        billRepository.updateBill(bill);
+    }
+
+    public void deleteBill(int billId) {
+        billRepository.deleteBill(billId);
+    }
+
+    public List<Bill> getAllBills() {
+        return billRepository.getAllBills();
+    }
+
+    public Bill getBillById(int billId) {
+        return billRepository.getBillById(billId);
+    }
 }
