5549fac68358cfa6f4e994d3829703802cb55b93
Aryman Srivastava-2020184 <88674260+Aryman-Srivastava@users.noreply.github.com>
2024-08-10 14:44:59 +0530

    Add files via upload
    This bill manager Service, Repo and Model classes, Note that I have created a sample Bill Class for temporary basis, Kindly note the difference and let me know incase of any changes.

---
+++ BillManager/src/com/paypilot/billmngr/BillManagerModel.java
@@ -0,0 +1,19 @@
+package com.paypilot.billmngr;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class BillManagerModel {
+	
+	private List<Bill> allBils = new ArrayList<>();
+	
+	public List<Bill> getAllBills(){
+		return this.allBils;
+	}
+	
+	public void setAllBills(List<Bill> b){
+		this.allBils = b;
+	}
+
+}
