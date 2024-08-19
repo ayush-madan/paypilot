9164f3b8941561b0b4b4b0550650582123802768
Aryman Srivastava-2020184 <88674260+Aryman-Srivastava@users.noreply.github.com>
2024-08-19 10:55:15 +0530

    Update BillManagerModel.java

--- src/com/paypilot/model/BillManagerModel.java
+++ src/com/paypilot/model/BillManagerModel.java
@@ -15,15 +15,24 @@ import java.util.List;
 
 public class BillManagerModel {
 	
-	// List to hold all bills
+	// List to hold all Bill objects
 	private List<Bill> allBils = new ArrayList<>();
-	
+
+	/**
+     	* Retrieves the current list of all bills.
+     	* 
+     	* @return A list of all Bill objects
+     	*/
 	public List<Bill> getAllBills(){
 		return this.allBils;
 	}
-	
+
+	/**
+     	* Updates the list of all bills with the provided list.
+     	* 
+     	* @param bills The list of Bill objects to set
+     	*/
 	public void setAllBills(List<Bill> b){
 		this.allBils = b;
 	}
-
 }
